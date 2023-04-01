5ef2a1843c3f4acbbaa82b7f8df974323e54dbf9
charliechocho <mattiassoderberj@gmail.com>
2023-04-01 15:18:12 +0200

    Updated hello.py

--- flasky/hello.py
+++ flasky/hello.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app_route('/')
+@app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}! '.format(name.title())
+
